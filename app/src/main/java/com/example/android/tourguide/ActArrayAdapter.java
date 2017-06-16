020bbcc5845ac7af890c48675cb003d442b3289d
Lyman <lyman@conxxeMBA.local>
2017-06-16 12:34:32 +0800

    update ViewHolder and tool:text

--- app/src/main/java/com/example/android/tourguide/ActArrayAdapter.java
+++ app/src/main/java/com/example/android/tourguide/ActArrayAdapter.java
@@ -18,6 +18,13 @@ import java.util.List;
 
 public class ActArrayAdapter extends ArrayAdapter<ActClass> {
 
+    static class ActHolder{
+        TextView activityName;
+        TextView activityTime;
+        TextView activityDescription;
+        ImageView activityLogo;
+    }
+
     public ActArrayAdapter(Context context, List<ActClass> objects){
         super(context,0,objects);
     }
@@ -26,28 +33,36 @@ public class ActArrayAdapter extends ArrayAdapter<ActClass> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        //define view holder to reduce usage of findViewByID
+        ActHolder actHolder;
+
         //listActivityView is list_activities which is used to store customized activity layout
         View listActivityView = convertView;
+
+        //initialize view holder
         if(listActivityView == null) {
             listActivityView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_activities, parent, false);
+
+            actHolder = new ActHolder();
+            actHolder.activityName = (TextView) listActivityView.findViewById(R.id.activity_name);
+            actHolder.activityTime = (TextView) listActivityView.findViewById(R.id.activity_time);
+            actHolder.activityDescription = (TextView) listActivityView.findViewById(R.id.activity_description);
+            actHolder.activityLogo = (ImageView) listActivityView.findViewById(R.id.activity_logo);
+
+            listActivityView.setTag(actHolder);
+        } else {
+            actHolder = (ActHolder) listActivityView.getTag();
         }
 
         //find the current activity to get its info with getter method and assign to convertView components
         //定位当前的Activity并用getter取得其值，然后赋给convertView响应的TextView和ImageView，以便进一步填充到ListView中进行展示
         ActClass currentActivity = getItem(position);
 
-        TextView activityName = (TextView) listActivityView.findViewById(R.id.activity_name);
-        activityName.setText(currentActivity.getActivityName());
-
-        TextView activityTime = (TextView) listActivityView.findViewById(R.id.activity_time);
-        activityTime.setText(currentActivity.getActivityTime());
-
-        TextView activityDescription = (TextView) listActivityView.findViewById(R.id.activity_description);
-        activityDescription.setText(currentActivity.getActivityDescription());
-
-        ImageView activityLogo = (ImageView) listActivityView.findViewById(R.id.activity_logo);
-        activityLogo.setImageResource(currentActivity.getActivityLogoID());
+        actHolder.activityName.setText(currentActivity.getActivityName());
+        actHolder.activityTime.setText(currentActivity.getActivityTime());
+        actHolder.activityDescription.setText(currentActivity.getActivityDescription());
+        actHolder.activityLogo.setImageResource(currentActivity.getActivityLogoID());
 
         //将重新赋值的convertView返回给ListView进行展示
         return listActivityView;
--- app/src/main/java/com/example/android/tourguide/ResArrayAdapter.java
+++ app/src/main/java/com/example/android/tourguide/ResArrayAdapter.java
@@ -21,38 +21,51 @@ public class ResArrayAdapter extends ArrayAdapter<ResClass>{
         super(context,0,objects);
     }
 
+    static class ResHolder{
+        TextView activityName;
+        TextView activityTime;
+        TextView activityDescription;
+        TextView activityStar;
+        TextView activityAddress;
+        ImageView activityLogo;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        ResHolder resHolder;
+
         //listActivityView is list_activities which is used to store customized activity layout
         View listActivityView = convertView;
+
         if(listActivityView == null) {
             listActivityView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_restaurant, parent, false);
+
+            resHolder = new ResHolder();
+            resHolder.activityName = (TextView) listActivityView.findViewById(R.id.restaurant_name);
+            resHolder.activityTime = (TextView) listActivityView.findViewById(R.id.restaurant_time);
+            resHolder.activityDescription = (TextView) listActivityView.findViewById(R.id.restaurant_flavor);
+            resHolder.activityStar = (TextView) listActivityView.findViewById(R.id.restaurant_star);
+            resHolder.activityAddress = (TextView) listActivityView.findViewById(R.id.restaurant_address);
+            resHolder.activityLogo = (ImageView) listActivityView.findViewById(R.id.restaurant_logo);
+
+            listActivityView.setTag(resHolder);
+        } else {
+            resHolder = (ResHolder) listActivityView.getTag();
         }
 
         //find the current activity to get its info with getter method and assign to convertView components
         //定位当前的Activity并用getter取得其值，然后赋给convertView响应的TextView和ImageView，以便进一步填充到ListView中进行展示
         ResClass currentActivity = getItem(position);
 
-        TextView activityName = (TextView) listActivityView.findViewById(R.id.restaurant_name);
-        activityName.setText(currentActivity.getResName());
-
-        TextView activityTime = (TextView) listActivityView.findViewById(R.id.restaurant_time);
-        activityTime.setText(currentActivity.getResTime());
-
-        TextView activityDescription = (TextView) listActivityView.findViewById(R.id.restaurant_flavor);
-        activityDescription.setText(currentActivity.getResFlavor());
-
-        TextView activityStar = (TextView) listActivityView.findViewById(R.id.restaurant_star);
-        activityStar.setText(currentActivity.getResStar());
-
-        TextView activityAddress = (TextView) listActivityView.findViewById(R.id.restaurant_address);
-        activityAddress.setText(currentActivity.getResAddress());
-
-        ImageView activityLogo = (ImageView) listActivityView.findViewById(R.id.restaurant_logo);
-        activityLogo.setImageResource(currentActivity.getResLogoID());
+        resHolder.activityName.setText(currentActivity.getResName());
+        resHolder.activityTime.setText(currentActivity.getResTime());
+        resHolder.activityDescription.setText(currentActivity.getResFlavor());
+        resHolder.activityStar.setText(currentActivity.getResStar());
+        resHolder.activityAddress.setText(currentActivity.getResAddress());
+        resHolder.activityLogo.setImageResource(currentActivity.getResLogoID());
 
         //将重新赋值的convertView返回给ListView进行展示
         return listActivityView;
